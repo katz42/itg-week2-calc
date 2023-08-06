4f40b5f6d79b78cd92cd0853f9ff3d6e113c057d
ekaterina <zhidkova.ea@gmail.com>
2023-08-06 19:25:34 +0300

    calc fix

--- src/main/java/ru/zhidkova/calcapp/service/ResultWriterService.java
+++ src/main/java/ru/zhidkova/calcapp/service/ResultWriterService.java
@@ -1,6 +1,22 @@
 package ru.zhidkova.calcapp.service;
 import ru.zhidkova.calcapp.util.Calculator;
 
-public class ResultWriterService {
+import java.util.Scanner;
 
+public class ResultWriterService {
+    public static void showResult(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int num1 = scanner.nextInt();
+        System.out.println("Введите второе число:");
+        int num2 = scanner.nextInt();
+
+        int sumResult = Calculator.sum(num1, num2);
+        int substractResult = Calculator.subtract(num1, num2);
+        int multiplyResult = Calculator.multiply(num1, num2);
+
+        System.out.println("Сумма " + num1 + " и " + num2 +  " будет равна " + sumResult + ".");
+        System.out.println("Разница " + num1 + " и " + num2 + " будет равна " + substractResult + ".");
+        System.out.println("При умножении " + num1 + " и " + num2 + " результат будет равен " + multiplyResult + ".");
     }
+}
--- src/main/java/ru/zhidkova/calcapp/util/Calculator.java
+++ src/main/java/ru/zhidkova/calcapp/util/Calculator.java
@@ -1,16 +1,16 @@
 package ru.zhidkova.calcapp.util;
 
 public class Calculator {
-    public static double sum(double num1, double num2) {
-        double sumResult = num1 + num2;
+    public static int sum(int num1, int num2) {
+        int sumResult = num1 + num2;
         return sumResult;
     };
-    public static double subtract(double num1, double num2) {
-        double substractResult = num1 - num2;
+    public static int subtract(int num1, int num2) {
+        int substractResult = num1 - num2;
         return substractResult;
     };
-    public static double multiply(double num1, double num2) {
-        double multiplyResult = num1 * num2;
+    public static int multiply(int num1, int num2) {
+        int multiplyResult = num1 * num2;
         return multiplyResult;
     };
 }
