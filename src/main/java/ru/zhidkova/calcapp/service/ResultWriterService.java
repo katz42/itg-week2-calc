46e849eda7bb4261a516accd41340dfa6abbeaff
ekaterina <zhidkova.ea@gmail.com>
2023-08-06 19:48:05 +0300

    add operationSymbol

--- src/main/java/ru/zhidkova/calcapp/service/ResultWriterService.java
+++ src/main/java/ru/zhidkova/calcapp/service/ResultWriterService.java
@@ -10,13 +10,21 @@ public class ResultWriterService {
         int num1 = scanner.nextInt();
         System.out.println("Введите второе число:");
         int num2 = scanner.nextInt();
+        System.out.println("Введите символ операции (+, -, *):");
+        String operationSymbol = scanner.next();
 
         int sumResult = Calculator.sum(num1, num2);
         int subtractResult = Calculator.subtract(num1, num2);
         int multiplyResult = Calculator.multiply(num1, num2);
 
-        System.out.println("Сумма " + num1 + " и " + num2 +  " будет равна " + sumResult + ".");
-        System.out.println("Разница " + num1 + " и " + num2 + " будет равна " + subtractResult + ".");
-        System.out.println("При умножении " + num1 + " и " + num2 + " результат будет равен " + multiplyResult + ".");
+        if (operationSymbol.contains("+")) {
+            System.out.println("Сумма " + num1 + " и " + num2 +  " будет равна " + sumResult + ".");
+        } else if (operationSymbol.contains("-")) {
+            System.out.println("Разница " + num1 + " и " + num2 + " будет равна " + subtractResult + ".");
+        } else if (operationSymbol.contains("*")) {
+            System.out.println("При умножении " + num1 + " и " + num2 + " результат будет равен " + multiplyResult + ".");
+        }
+        else
+            System.out.println("Введён некорректный символ операции");
     }
 }
